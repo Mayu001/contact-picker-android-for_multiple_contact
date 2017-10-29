a28bb85f870506681f38d7e4460a49dc0e285055
Mayank Mohit <mayank.mohit1122@gmail.com>
2017-10-30 02:16:11 +0530

    Backend completed for picking the contacts and assigning the values to textfield

--- app/src/main/java/com/example/mayu/contactpicker_topickmultiplecontact/MainActivity.java
+++ app/src/main/java/com/example/mayu/contactpicker_topickmultiplecontact/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import static android.app.Activity.RESULT_OK;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int RESULT_PICK_CONTACT=1;
@@ -23,4 +25,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void PickContact(View view)
+    {
+        if(view.getId()==R.id.PickContactButton1) {
+            Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+            startActivityForResult(i, RESULT_PICK_CONTACT);
+            PICK=1;
+        }
+        else if(view.getId()==R.id.PickContactButton2)
+        {
+            Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+            startActivityForResult(i, RESULT_PICK_CONTACT);
+            PICK=2;
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Log.e("MainActivity", "Failed to pick contact");
+        }
+    }
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+        try {
+            Uri uri = data.getData();
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            int  phoneIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            int  nameIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            String phoneNo = cursor.getString(phoneIndex);
+            String name = cursor.getString(nameIndex);
+            EditText ed1=(EditText)findViewById(R.id.Name1);
+            EditText ed2=(EditText)findViewById(R.id.Phone1);
+
+            EditText ed3=(EditText)findViewById(R.id.Name2);
+            EditText ed4=(EditText)findViewById(R.id.Phone2);
+            if(PICK==1) {
+                ed2.setText(phoneNo);
+                ed1.setText(name);
+            }
+            else if(PICK==2)
+            {
+                ed4.setText(phoneNo);
+                ed3.setText(name);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            cursor.close();
+        }
+    }
 }
